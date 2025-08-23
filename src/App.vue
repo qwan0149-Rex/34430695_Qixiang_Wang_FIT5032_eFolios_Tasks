332213cb1b25f91c3dc94ffb17752995478ac2da
qwan0149-Rex <qwan0149@student.monash.edu>
2025-08-23 17:17:32 +1000

    This is my eFolios Tasks codes(Week1-4)

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import JSONLab from './components/JSON.vue'
+import Form from './components/Form.vue';
+</script>
+
+<template>
+  <!-- <JSONLab /> -->
+   <Form />
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,21 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+createApp(App).mount('#app')
