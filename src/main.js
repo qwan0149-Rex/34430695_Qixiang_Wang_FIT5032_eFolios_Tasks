3138d953b37bb140684d13f574bb3b979eef82c7
qwan0149-Rex <qwan0149@student.monash.edu>
2025-09-12 23:39:08 +1000

    Firebase set up

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import Aura from '@primeuix/themes/aura'
 // import DataTable from 'primevue/datatable'
 // import Column from 'primevue/column'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
 const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
@@ -19,6 +22,22 @@ app.use(PrimeVue, {
 
 // app.component('DataTable', DataTable)
 // app.component('Column', Column)
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCLPanEdcgKAJGJerD9Bs8PRWm_oNjdh0U',
+  authDomain: 'week7-qixiang-wang.firebaseapp.com',
+  projectId: 'week7-qixiang-wang',
+  storageBucket: 'week7-qixiang-wang.firebasestorage.app',
+  messagingSenderId: '639379896871',
+  appId: '1:639379896871:web:2fb5ca10bb9c9634272c02',
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
 
 app.use(router)
 app.mount('#app')
